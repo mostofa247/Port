98972cf334071c1ab50afc0b8864382bcbe1874a
mostofa247 <74599998+mostofa247@users.noreply.github.com>
2021-01-15 11:30:12 -0500

    few change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3c#$l6la&581el1r^hmmvtl0h#(k#x!fu70!&%do@+@097egr4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
